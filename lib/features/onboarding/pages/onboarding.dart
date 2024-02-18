7bf2287986d527483af98945c28f7432f69dd632
JesbinGit <jesbinytofficial@gmail.com>
2024-02-18 13:30:17 +0530

    Added page Controller or Scroll things

--- lib/features/onboarding/pages/onboarding.dart
+++ lib/features/onboarding/pages/onboarding.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
 
-class OnBoarding extends StatelessWidget {
+class OnBoarding extends StatefulWidget {
   const OnBoarding({super.key});
 
   @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  final PageController pageController = PageController();
+
+
+  @override
+  void dispose()
+  {
+    pageController.dispose();
+    super.dispose();
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [PageView()],
+        children: [
+          PageView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          controller: ,
+        )
+        
+        ],
       ),
     );
   }
